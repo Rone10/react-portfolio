40af8854cb1c2930613523d4e7f5129f79d08d81
Haruna <harunanjie@hotmail.com>
2021-01-14 05:31:45 -0800

    landing page retouch

--- src/components/data/landing.js
+++ src/components/data/landing.js
@@ -1,67 +1,14 @@
 import { Link } from 'react-router-dom'
 
+
 const content = () => {
     return (
-        <div id="home" className="text-white mt-16 pt-20">
-            <div className="landing h-30rem flex flex-grow">
-                <div className=" flex-1 mr-0">
-                    <h1 className="mr-0">Photo</h1>
-                </div>
-                <div className="flex flex-col w-3/5 p-20">
-                    <h1 className="font-bold text-5xl">Haruna Njie</h1>
-                    <h3 className="mt-1 text-3xl">Full-stack Developer</h3>
+        <div id="home" className="text-whites mt-10 w-screen h-screen">
+            <div className="bg-hero-landing bg-auto bg-center w-full h-full" >
+                <div className="w-full h-full bg-black bg-opacity-50 "><h1 className="text-white text-5xl  text-center ">Haruna Njie</h1></div>
+            </div>
+            <span>Photo by <a href="https://unsplash.com/@joshuaryanphoto?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Joshua Reddekopp</a> on <a href="https://unsplash.com/s/photos/coding?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
 
-                </div>
-            </div>
-            <div className="flex flex-col w-3/5 p-20">
-                <Link activeClass="active" to="/projects" className="link-tag text-black">
-                    Projects
-            </Link>
-            </div>
-
-            <div className="bio w-4/5 mx-auto text-black my-14">
-                <h1>About Me</h1>
-                <p>  am Junior Web developer able to build a Web
-                presence from the ground up - from concept,
-                navigation, layout and programming to UX and SEO.
-                Skilled at writing well-designed, testable and efficient
-                code using current best practices in Web development. Fast
-                learner, hard worker and team player who is proficient in an
-                array of scripting languages and multimedia Web tools.
-    </p>
-                <p>  am Junior Web developer able to build a Web
-                presence from the ground up - from concept,
-                navigation, layout and programming to UX and SEO.
-                Skilled at writing well-designed, testable and efficient
-                code using current best practices in Web development. Fast
-                learner, hard worker and team player who is proficient in an
-                array of scripting languages and multimedia Web tools.
-    </p>
-                <p>  am Junior Web developer able to build a Web
-                presence from the ground up - from concept,
-                navigation, layout and programming to UX and SEO.
-                Skilled at writing well-designed, testable and efficient
-                code using current best practices in Web development. Fast
-                learner, hard worker and team player who is proficient in an
-                array of scripting languages and multimedia Web tools.
-    </p>
-                <p>  am Junior Web developer able to build a Web
-                presence from the ground up - from concept,
-                navigation, layout and programming to UX and SEO.
-                Skilled at writing well-designed, testable and efficient
-                code using current best practices in Web development. Fast
-                learner, hard worker and team player who is proficient in an
-                array of scripting languages and multimedia Web tools.
-    </p>
-                <p>  am Junior Web developer able to build a Web
-                presence from the ground up - from concept,
-                navigation, layout and programming to UX and SEO.
-                Skilled at writing well-designed, testable and efficient
-                code using current best practices in Web development. Fast
-                learner, hard worker and team player who is proficient in an
-                array of scripting languages and multimedia Web tools.
-    </p>
-            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundImage: theme => ({
+        'hero-landing': "url('/src/images/hero-image.jpg')",
+      }),
       spacing: {
         '30rem': '30rem'
       },
